bd5e6ca2964ed23e51b3fb6057bd959d55a81dcd
Alex Pinedo <31834217+rocketmanhs@users.noreply.github.com>
2019-03-27 16:48:25 -0700

    Pushin Updates

---
+++ Senior Project 491A/Assets/_Scripts/Enemy/EnemyCard.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCard
+{
+    private int rewardValue;
+    private int healthValue;
+    
+    
+}
