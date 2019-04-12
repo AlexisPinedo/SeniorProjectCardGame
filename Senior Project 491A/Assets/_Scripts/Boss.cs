c03a5b6aa4856ec17511d4fd0be55b737a5e2bd8
David Taitingfong <djtai@protonmail.com>
2019-04-11 22:22:36 -0700

    Create IEnemy, Boss, Minion, and Goal scripts
    IEnemy is an interface; Boss, Minion, and Goal are classes

---
+++ Senior Project 491A/Assets/_Scripts/Boss.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour, IEnemy
+{
+    // from IEnemy
+    public int health { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public int rewardValue { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    public Goal goal;
+
+    // TODO: Change to classes
+    public string deck;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO: Set the goal?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Senior Project 491A/Assets/_Scripts/Goal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void EndGame()
+    {
+        // TODO: Change from a debug log to an end state
+        Debug.Log("Goal was reached. Boss has won. Sorry bud");
+    }
+}
