4d9755b2575ad537b7dbcf0aa44c20ecb7e7c918
David Taitingfong <djtai@protonmail.com>
2019-04-28 10:26:37 -0700

    Fix connections between Deck, Hand, and Player
    Sequence of events: player chooses to add card to their hand -> hand increases by drawing a card from the deck -> the deck pops the top card off and into the player's hand.

--- Senior Project 491A/Assets/_Scripts/Hand.cs
+++ Senior Project 491A/Assets/_Scripts/Hand.cs
@@ -2,31 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hand : ScriptableObject
+public class Hand : MonoBehaviour
 {
-    private List<Card> playerCards = new List<Card>();
-    private Deck deck = ScriptableObject.CreateInstance<Deck>();
-    
-    //public Card playCard()
-    //{
-    //    Card selectedCard;
-    //    
-    //    return selectedCard;
-    //}
-    
-    public void DrawCard()
+    private static int handSize = 6;
+
+    [SerializeField]
+    // private List<Card> hand = new List<Card>(handSize);
+    private List<Card> hand;
+
+    private int cardsInHand = 0;
+
+    [SerializeField]
+    private Deck deck;
+
+    private CreateGrid handGrid;
+
+    /* Spawn point for car */
+    private Vector2 spot = new Vector2();
+
+    void Start()
     {
-        playerCards.Add(deck.DrawCard());
+        // TODO: Verify if this fix is suitable
+        // Create GameObjects to find them by their name
+        GameObject goDeck = GameObject.Find("Player1Deck");
+        GameObject goHandGrid = GameObject.Find("Player1CardGrid");
+        
+        // Reference the player's components
+        if (goDeck != null)
+        {
+            deck = goDeck.GetComponent<Deck>();
+            print("Deck: " + deck);
+        }
+        if (goHandGrid != null)
+        {
+            handGrid = goHandGrid.GetComponent<CreateGrid>();
+            print("Grid: " + handGrid);
+        }
     }
-    
+
+    public void AddCard()
+    {
+        // Draw card from deck and add it to the Hand's list
+        Card cardDrawn;
+        Vector2 spawnPoint;
+
+        cardDrawn = deck.DrawCard();
+        Debug.Log(cardDrawn.ToString());  // DEBUG
+        hand.Add(cardDrawn);
+
+        spawnPoint = handGrid.GetNearestPointOnGrid(spot);
+        cardDrawn.transform.position = spawnPoint;
+        spot.x += 2.0f;
+
+        cardsInHand += 1;
+    }
+
     public List<Card> GetHand()
     {
-        return playerCards;
+        return hand;
     }
 
     public void DiscardCard(Card card)
     {
-        if (playerCards.Contains(card))
-            playerCards.Remove(card);
+        if (hand.Contains(card))
+        {
+            hand.Remove(card);
+            cardsInHand -= 1;
+        }
+    }
+
+    public int getHandCount()
+    {
+        return this.cardsInHand;
     }
 }
