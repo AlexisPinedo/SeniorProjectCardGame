e163185bd6fdf0bffb67303d250e0a8a8d5cd1ce
ZackMinott <zacharyminott1997@gmail.com>
2019-04-29 14:17:08 -0700

    Made card draggable

---
+++ Senior Project 491A/Assets/_Scripts/DragCard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class DragCard : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position); //used to grab the z coordinate of the game object 
+
+        offset = gameObject.transform.position -
+                 Camera.main.ScreenToWorldPoint(
+                     new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 cursorScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z); //stores position of cursor in screen space
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorScreenPoint) + offset; //grabs the position of the mouse cursor and converts to world space
+
+        transform.position = cursorPosition; //updates position of game object
+    }
+}
