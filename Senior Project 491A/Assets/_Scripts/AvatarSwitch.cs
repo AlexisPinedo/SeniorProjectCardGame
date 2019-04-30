dad63bcc70ec2e6c4f96b76ec60c5595fc60844e
Dominique Oyco <dominickoyco@gmail.com>
2019-04-30 15:54:37 -0700

    Update UI Elements

---
+++ Senior Project 491A/Assets/_Scripts/AvatarSwitch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AvatarSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject p1Avatar;
+
+    [SerializeField]
+    private GameObject p2Avatar;
+
+    [SerializeField]
+    private TurnManager turnManager;
+
+    void Start()
+    {
+        p2Avatar.SetActive(false);
+    }
+
+    public void ShowHideAvatar()
+    {
+        if (p1Avatar != null && p2Avatar != null)
+        {
+            // Switch to Player Two
+            if (p1Avatar.activeSelf)
+            {
+                p1Avatar.SetActive(false);
+                p2Avatar.SetActive(true);
+                turnManager.SetPlayerTwosTurn();
+            }
+            // Switch to Player One
+            else if (p2Avatar.activeSelf)
+            {
+                p2Avatar.SetActive(false);
+                p1Avatar.SetActive(true);
+                turnManager.SetPlayerOnesTurn();
+            }
+        }
+    }
+}
