20c06a48774dea8c076632facb6826992d50d88a
Alex Pinedo <31834217+rocketmanhs@users.noreply.github.com>
2019-05-02 10:39:42 -0700

    Updating scenes and beginning to work on card log for effect evaluation

---
+++ Senior Project 491A/Assets/_Scripts/Card Log/CardLog.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardLog : MonoBehaviour
+{
+    [SerializeField]
+    private List<Card> cardHistory = new List<Card>();
+
+
+}
