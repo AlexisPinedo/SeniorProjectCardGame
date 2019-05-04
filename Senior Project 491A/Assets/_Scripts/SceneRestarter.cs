d1576b1a0d0dc89651a14200b39e5ecefb05ef33
Dominique Oyco <dominickoyco@gmail.com>
2019-05-04 13:20:54 -0700

    Added a restart button UI component

---
+++ Senior Project 491A/Assets/_Scripts/SceneRestarter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneRestarter : MonoBehaviour
+{
+    public void RestartScene()
+    {
+        SceneManager.LoadSceneAsync("RainScene (Duplicate)");
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadSceneAsync("RainScene (Duplicate)");
+        }
+    }
+}
