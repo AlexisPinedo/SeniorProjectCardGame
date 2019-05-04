01c61c53c16bb344e357fdd8ea482e6940c1e7c4
Dominique Oyco <dominickoyco@gmail.com>
2019-05-04 15:33:32 -0700

    update UI elements

--- Senior Project 491A/Assets/_Scripts/SceneRestarter.cs
+++ Senior Project 491A/Assets/_Scripts/SceneRestarter.cs
@@ -6,13 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class SceneRestarter : MonoBehaviour
 {
-    public void RestartScene()
+    void Update()
     {
-        SceneManager.LoadSceneAsync("RainScene (Duplicate)");
-
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             SceneManager.LoadSceneAsync("RainScene (Duplicate)");
         }
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadSceneAsync("RainScene (Duplicate)");
     }
 }
