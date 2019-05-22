972e4b8f8293b3ad5e65a071e36eb50c19655fe1
DJTai <djtai@protonmail.com>
2019-05-22 16:23:48 -0700

    Add PurchaseHandler.cs to Shop dir

---
+++ Senior Project 491A/Assets/_Scripts/Shop/PurchaseHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseHandler : MonoBehaviour
+{
+    public delegate void _CardBought(Card cardBuying);
+    public static event _CardBought CardBought;
+
+    private TurnManager turnManager;
+    private Player turnPlayer;
+
+    public void Start()
+    {
+        turnPlayer = turnManager.turnPlayer;
+    }
+
+    public bool isPurchasable(Card cardClicked)
+    {
+        bool canBePurchased;
+        // TODO
+
+        canBePurchased = true;
+
+        return canBePurchased;
+    }
+
+    public void PurchaseCard(Card cardBuying)
+    {
+        // ?? Invoke ??
+    }
+
+}
