873df86a6e7661147ddc1704102c546217842177
DJTai <djtai@protonmail.com>
2019-08-28 12:42:42 -0700

    Add proper documentation.
    Voila! The documentation is so nice for these files.

--- Senior Project 491A/Assets/_Scripts/Card/Card.cs
+++ Senior Project 491A/Assets/_Scripts/Card/Card.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Represents the basic foundation of all Cards in the game, e.g., Player, Enemy, Boss, etc...
+/// </summary>
 public abstract class Card : MonoBehaviour
 {
+    /// <summary>
+    /// The specific name of the card.
+    /// </summary>
     public string cardName;
     
+    /// <summary>
+    /// Short description about the card, but not it's effect(s) in game.
+    /// </summary>
     public string cardDescription;
 
+    /// <summary>
+    /// The Card's effect in game.
+    /// </summary>
     public string cardEffect;
 
+    /// <summary>
+    /// The artwork for the card, stored as a ???
+    /// </summary>
     public Sprite cardArtwork;
     
+    /// <summary>
+    /// The element, i.e. type, of the card.
+    /// </summary>
     public Sprite cardElement;
 }
--- Senior Project 491A/Assets/_Scripts/Enemy/Boss.cs
+++ Senior Project 491A/Assets/_Scripts/Enemy/Boss.cs
@@ -9,58 +9,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-/*
-    This class represents a match's Boss
- */
+/// <summary>
+/// A Boss card represents ...well... the boss of a level.
+/// </summary>
 public class Boss : EnemyCard
 {
-    /* Boss' health and reward value */
-    public int _health;
+    /// <summary>
+    /// The Boss' health. Can't be lowered, only obliterated!
+    /// </summary>
+    private readonly int _health;
 
-    //// from IEnemy
-    //public int health
-    //{
-    //    get { return _health; }
-    //    set { _health = value; }
-    //}
-    //public int rewardValue
-    //{
-    //    get { return _rewardValue;}
-    //    set { _rewardValue = value;}
-    //}
-
-    /* The Boss' Goal */
+    /// <summary>
+    /// What the Boss needs to have happen in the game in order for him/her/it to win!
+    /// </summary>
     public Goal goal;
 
-    /* The Boss' Deck */
+    /// <summary>
+    /// The Boss' deck of cards (they are indeed different than yours).
+    /// </summary>
     public EnemyDeck bossDeck;
 
+    /// <summary>
+    /// A reference to the Enemy's grid.
+    /// </summary>
     public CreateGrid EnemyGrid;
+
     public BossTurnCardPlayer cardPlayer;
 
-    // Start is called before the first frame update
     void Start()
     {
         // TODO: Set the goal?
         // TODO: Populate the Boss' deck?
 
         this.transform.position = EnemyGrid.GetNearestPointOnGrid(new Vector2(6, 2));
-
     }
 
     public override void OnMouseDown()
     {
         if (cardPlayer.filledCardZones == 0)
         {
-            Debug.Log("can attack boss");
-            if (TurnManager.turnPlayer.GetPower() >= _health)
+            Debug.Log("Player can attack the Boss");
+            if (TurnManager.turnPlayer.GetPower() >= this._health)
             {
                 Destroy(this.gameObject);
             }
             else
             {
-                Debug.Log("not enough power");
-
+                Debug.Log("Player lacks the power to defeat the Boss!");
             }
 
         }
