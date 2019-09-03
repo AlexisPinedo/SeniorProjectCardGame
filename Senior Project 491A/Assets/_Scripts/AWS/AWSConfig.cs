5d269c1e3f66262153e7040ec735fb1d013d860b
ZackMinott <zacharyminott1997@gmail.com>
2019-09-03 09:36:11 -0700

    implemented necessary AWS SDKs, created Cognito User Pool and Identity Pool for email sign up/ sign-in, added initial configurations to retrieve the pool from AWS

---
+++ Senior Project 491A/Assets/_Scripts/AWS/AWSConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Amazon;
+using Amazon.CognitoIdentity;
+using UnityEngine;
+
+public class AWSConfig : MonoBehaviour
+{
+
+    CognitoAWSCredentials credentials = new CognitoAWSCredentials(
+        "us-west-2:03e86ab6-282d-4c8d-9f75-77849ce61e96", // Identity pool ID
+        RegionEndpoint.USWest2 // Region
+    );
+
+    void Awake()
+    {
+        UnityInitializer.AttachToGameObject(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
