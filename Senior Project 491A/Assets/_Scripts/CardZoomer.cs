63c7066c2f7ce44ab37eac3a1e3d1552eb38efb5
Dominique Oyco <dominickoyco@gmail.com>
2019-09-16 13:11:38 -0700

    implemented zoom feature

--- Senior Project 491A/Assets/_Scripts/CardZoomer.cs
+++ Senior Project 491A/Assets/_Scripts/CardZoomer.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class CardZoomer : MonoBehaviour
 {
+    private object myGameObject;
+
+    public Vector2 OriginalPosition;
+
+    private void Awake()
+    {
+        OriginalPosition = this.transform.position;
+    }
+
     public void OnMouseEnter()
     {
-        Debug.Log("enter");
-        //transform.localScale += new Vector3(1.1F, 1.1f, 1.1f); //zooms in the object
+        //Debug.Log("enter");
+        transform.localScale += new Vector3(1.5F, 1.5F, 1.5F); //zooms in the object
     }
 
     public void OnMouseExit()
     {
-        Debug.Log("exit");
-        //transform.localScale = new Vector3(1, 1, 1);  //returns the object to its original state
+        //Debug.Log("exit");
+        transform.localScale = new Vector3(1, 1, 1);  //returns the object to its original state
     }
 }
