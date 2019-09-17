c3b124f0c7c0d4732e03ee1e595f1a4ae4671921
Edward <ed@edwardvuong.com>
2019-09-16 21:30:32 -0700

    Update Lobby Scene
    Add main lobby and player lobby. Master client connection that allows control of match.

---
+++ Senior Project 491A/Assets/_Scripts/Lobby/RoomListingsView.cs
@@ -0,0 +1,54 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomListingsView : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Transform _content;
+    [SerializeField]
+    private RoomListing _roomListing;
+
+    private List<RoomListing> _listings = new List<RoomListing>();
+
+    //public override void OnJoinedRoom()
+    //{
+    //    //_roomsCanvases.CurrentRoomCanvas.Show();
+    //    //_content.DestroyChildren();
+    //    _listings.Clear();
+    //}
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (RoomInfo info in roomList)
+        {
+            //Removed from rooms list.
+            if (info.RemovedFromList)
+            {
+                int index = _listings.FindIndex(x => x.RoomInfo.Name == info.Name);
+                if (index != -1)
+                {
+                    Destroy(_listings[index].gameObject);
+                    _listings.RemoveAt(index);
+                }
+            }
+
+            //Added to rooms list.
+            else
+            {
+                int index = _listings.FindIndex(x => x.RoomInfo.Name == info.Name);
+                if (index == -1)
+                {
+                    RoomListing listing = Instantiate(_roomListing, _content);
+                    if (listing != null)
+                    {
+                        listing.SetRoomInfo(info);
+                        _listings.Add(listing);
+                    }
+                }
+            }
+        }
+    }
+}
