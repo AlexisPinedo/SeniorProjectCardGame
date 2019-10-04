1df9892f4457fd91afa124c41b10ba37642a16bb
Edward <ed@edwardvuong.com>
2019-10-04 15:18:08 -0700

    PhotonViews

---
+++ Senior Project 491A/Assets/PhotonNetworkManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonNetworkManager : MonoBehaviourPunCallbacks
+{
+
+    [SerializeField]
+    GameObject player1Prefab, player2Prefab, shopPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Online Gameplay
+        if (PhotonNetwork.IsConnected)
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                PhotonNetwork.Instantiate(player1Prefab.name, new Vector3(0, 0, 0), Quaternion.identity);
+                PhotonNetwork.Instantiate(shopPrefab.name, new Vector3(0, 0, 0), Quaternion.identity);
+            }
+            else
+            {
+                PhotonNetwork.Instantiate(player2Prefab.name, new Vector3(0, 0, 0), Quaternion.identity);
+            }
+        }
+        //Offline Gameplay
+        else
+        {
+
+        }
+    }
+
+}
