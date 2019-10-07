8a58c23f379a98f510952efaf28d6b39f5584ed5
Alex Pinedo <31834217+AlexisPinedo@users.noreply.github.com>
2019-10-07 13:37:08 -0700

    Fixed UI handler

--- Senior Project 491A/Assets/_Scripts/UI/UIHandler.cs
+++ Senior Project 491A/Assets/_Scripts/UI/UIHandler.cs
@@ -22,11 +22,45 @@ public class UIHandler : MonoBehaviourPunCallbacks
 
     public delegate void EndTurnButtonAction();
     public static event EndTurnButtonAction EndTurnClicked;
+    
+        
+    public delegate void _notificationWindowEnabled();
+
+    public static event _notificationWindowEnabled NotificationWindowEnabled;
+    
+
+    private static UIHandler _instance;
+
+    public static UIHandler Instance
+    {
+        get { return _instance; }
+    }
+
+    [SerializeField]
+    private NotificationWindow windowReference;
 
     private void Awake()
     {
-        startBattleButton.SetActive(true);
-        endTurnButton.SetActive(true);
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        
+        if (photonView.IsMine)
+        {
+            startBattleButton.SetActive(true);
+            endTurnButton.SetActive(true);
+        }
+        else
+        {
+            startBattleButton.SetActive(true);
+            endTurnButton.SetActive(true);
+        }
+
 
         if (!PhotonNetworkManager.IsOffline)
         {
@@ -61,6 +95,14 @@ public class UIHandler : MonoBehaviourPunCallbacks
     public void EndTurnButtonOnClick()
     {
         EndTurnClicked?.Invoke();
+    }
+    
+    public void EnableNotificationWindow(string message)
+    {
+        windowReference.gameObject.SetActive(true);
+        NotificationWindow.Instance.DisplayMessage(message);
+        NotificationWindow.Instance.transparentCover.gameObject.SetActive(true);
+        NotificationWindowEnabled?.Invoke();
     }
 
 }
