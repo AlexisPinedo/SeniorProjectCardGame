0925c1182c891c591165db9ecee069c07a7efb57
ZackMinott <zacharyminott1997@gmail.com>
2019-10-09 12:35:00 -0700

    Finished Firebase Authentication and Database

--- Senior Project 491A/Assets/_Scripts/Firebase/Authentication/SignUp.cs
+++ Senior Project 491A/Assets/_Scripts/Firebase/Authentication/SignUp.cs
@@ -1,16 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SignUp : Forms
 {
-    [SerializeField] private InputField emailInput;
-    [SerializeField] private InputField passwordInput;
     [SerializeField] private InputField passwordRetype;
 
     [SerializeField] private GameObject emailPopUp;
+
+    private bool passAuthorized;
+
+    //Check Password Retype
+    public void ValidatePassword()
+    {
+        if (passwordInput.text != passwordRetype.text)
+        {
+            passAuthorized = false;
+            Debug.Log("Passwords are not equal");
+        }
+        else if (passwordInput.text == passwordRetype.text)
+        {
+            //bool hasUpper = passwordInput.text.Any(char.IsUpper);
+            //bool lengthStandard = passwordInput.text.Length >= 6;
+            //bool hasLower = passwordInput.text.Any(char.IsLower);
+            //bool hasNumber = passwordInput.text.Any(char.IsNumber);
+            //if (hasUpper && lengthStandard & hasLower & hasNumber)
+            //{
+            //    passAuthorized = true;
+            //}
+            //else
+            //{
+            //    passAuthorized = false;
+            //    Debug.Log("Password doesn't match the required criteria");
+            //    Debug.Log("Upper: " + hasUpper + " Lower: " + hasLower + " Length: " + lengthStandard + " Numbers: " + hasNumber);
+            //}
+            passAuthorized = true;
+        }
+    }
+
+    public void CanCreateAccount()
+    {
+        if (emailAuthorized && passAuthorized)
+            EmailSignUp();
+        else
+        {
+            if (!emailAuthorized && passAuthorized)
+                Debug.LogError("Email not valid");
+            else if (emailAuthorized && !passAuthorized)
+                Debug.LogError("Password does not meet valid criteria");
+            else
+                Debug.LogError("Both Email and Password are invalid");
+        }
+    }
+
+
 
     public void EmailRegistrationBtn(bool shouldOpen)
     {
@@ -20,7 +66,7 @@ public class SignUp : Forms
             emailPopUp.SetActive(false);
     }
 
-    public void EmailSignUp()
+    private void EmailSignUp()
     {
         AuthManager.sharedInstance.SignUpNewUserWithEmail(emailInput.text, passwordInput.text);
     }
