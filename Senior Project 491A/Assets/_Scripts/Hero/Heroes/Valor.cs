46b6d2b1073463ff74a5a97bfad158be657b11e6
Alex Pinedo <31834217+AlexisPinedo@users.noreply.github.com>
2019-10-11 13:25:03 -0700

    working on valor card effect

--- Senior Project 491A/Assets/_Scripts/Hero/Heroes/Valor.cs
+++ Senior Project 491A/Assets/_Scripts/Hero/Heroes/Valor.cs
@@ -9,6 +9,13 @@ public class Valor : CostRequirementHero
 {
     protected override void HeroPowerEffect()
     {
-        UIHandler.Instance.EnableNotificationWindow("Valor Effect Activated");
+        UIHandler.Instance.EnableNotificationWindow("Select up to 2 cards of the same color");
+        
+        
+    }
+
+    IEnumerable SelectionState()
+    {
+        
     }
 }
