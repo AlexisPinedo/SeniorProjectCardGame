05f01be51916203cc45da219402da84cee1888e9
ZackMinott <zacharyminott1997@gmail.com>
2019-10-17 20:40:05 -0700

    Finished AssetReferences?

--- Senior Project 491A/Assets/_Scripts/UI/ParticlePlay.cs
+++ Senior Project 491A/Assets/_Scripts/UI/ParticlePlay.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class ParticlePlay : MonoBehaviour
 {
+    [SerializeField] private AssetReference goldSummoner;
+    [SerializeField] private AssetReference purpleSummoner;
+
     private bool canDestroyCircle;
 
     // Start is called before the first frame update
@@ -22,6 +29,10 @@ public class ParticlePlay : MonoBehaviour
 
     IEnumerator PlaySummoningCircleHelper()
     {
+        Debug.Log("Playing Gold Summoner");
+
+        LoadAndSpawn(goldSummoner);
+
         yield return new WaitForSeconds(1.5f);
 
         if (!canDestroyCircle)
@@ -32,11 +43,32 @@ public class ParticlePlay : MonoBehaviour
 
     void PlayPurpleCircle()
     {
+        Debug.Log("Card Played.. Playing Purple Summoner");
+
         // Instantiate the PurpleCircleHere
+        LoadAndSpawn(purpleSummoner);
+        
+    }
+
+    void LoadAndSpawn(AssetReference assetReference)
+    {
+        var op = Addressables.LoadAssetAsync<GameObject>(assetReference);
+
+        op.Completed += (operation) =>
+        {
+            assetReference.InstantiateAsync(transform.position, Quaternion.identity).Completed +=
+                (asyncOperationHandle) =>
+                {
+                    Debug.Log("Spawned summoner");
+                   
+                };
+        };
     }
 
     void CanDestroySummoner()
     {
+        Debug.Log("Mouse Up, Card can be lifted");
+
         canDestroyCircle = true;
     }
 
