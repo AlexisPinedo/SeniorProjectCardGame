06578265c83e61bc48a550965faa94a56b788aa4
Dominique Oyco <dominickoyco@gmail.com>
2019-10-21 22:09:12 -0700

    update sorting layers (not fixed yet)

--- Senior Project 491A/Assets/_Scripts/Card/PlayerCardDisplay.cs
+++ Senior Project 491A/Assets/_Scripts/Card/PlayerCardDisplay.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System;
+using UnityEngine.Tilemaps;
 
 /// <summary>
 /// Holds visual information specific to PlayerCards. Extends CardDisplay.
@@ -32,22 +33,47 @@ public class PlayerCardDisplay : CardDisplay
     public delegate void _CardPurchased(PlayerCardDisplay cardBought);
 
     public static event _CardPurchased CardPurchased;
-    
-    public string sortingLayerName;
-    public int sortingOrder;
 
-    //for testing purposes
-    public string sortName;
-    public int sortOrder;
+//    public string sortLayerName;
+//    public int sortOrder;
+
+    //public GameObject[] cardParts;
+
+    public string imageLayerName;
+    public string typeLayerName;
+    public string attackLayerName;
+    public string soulLayerName;
+    public string costLayerName;
+    public string borderLayerName;
+    public string effectsLayerName;
+    public string nameLayer;
+    
+    public GameObject cardImage;
+    public GameObject cardTypeIcon;
+    public GameObject cardAttackIcon;
+    public GameObject cardSoulIcon;
+    public GameObject cardCostIcon;
+    public GameObject cardBorderImage;
+    public GameObject effectsTxt;
+    public GameObject nameTxt;
+
+    public int originalImageOrder;
+    public int originalTypeOrder;
+    public int originalAttackOrder;
+    public int originalSoulOrder;
+    public int originalCostOrder;
+    public int originalBorderOrder;
+    public int originaleffectOrder;
+    public int originalNameOrder;
 
     //private static List<int> photonViewIDs = new List<int>();
 
     //When the PlayerCardDisplay is loaded we want to load in the components into the display
-        protected override void Awake()
-        {
-            base.Awake();
-            LoadCardIntoDisplay();
-        }
+    protected override void Awake()
+    {
+        base.Awake();
+        LoadCardIntoDisplay();
+    }
 
     /// <summary>
     /// Called when this object is enabled. Adds EventReceived to the Networking Client.
@@ -103,8 +129,36 @@ public class PlayerCardDisplay : CardDisplay
                     Debug.Log("Unable to allocate ID");
                 }
             }
-            sortingLayerName = GetComponent<ParticleSystemRenderer>().sortingLayerName;
-            sortingOrder = GetComponent<ParticleSystemRenderer>().sortingOrder;
+
+            cardImage.GetComponent<Renderer>().sortingLayerName = imageLayerName;
+            cardTypeIcon.GetComponent<Renderer>().sortingLayerName = typeLayerName;
+            cardAttackIcon.GetComponent<Renderer>().sortingLayerName = attackLayerName;
+            cardSoulIcon.GetComponent<Renderer>().sortingLayerName = soulLayerName;
+            cardCostIcon.GetComponent<Renderer>().sortingLayerName = costLayerName;
+            cardBorderImage.GetComponent<Renderer>().sortingLayerName = borderLayerName;
+            effectsTxt.GetComponent<Renderer>().sortingLayerName = effectsLayerName;
+            nameTxt.GetComponent<Renderer>().sortingLayerName = nameLayer;
+            
+            cardImage.GetComponent<Renderer>().sortingOrder = originalImageOrder;
+            cardTypeIcon.GetComponent<Renderer>().sortingOrder = originalTypeOrder;
+            cardAttackIcon.GetComponent<Renderer>().sortingOrder = originalAttackOrder;
+            cardSoulIcon.GetComponent<Renderer>().sortingOrder = originalSoulOrder;
+            cardCostIcon.GetComponent<Renderer>().sortingOrder = originalCostOrder;
+            cardBorderImage.GetComponent<Renderer>().sortingOrder = originalBorderOrder;
+            effectsTxt.GetComponent<Renderer>().sortingOrder = originaleffectOrder;
+            nameTxt.GetComponent<Renderer>().sortingOrder = originalNameOrder;
+//            for (int i = 0; i < cardParts.Length; i++)
+//            {
+//                cardParts[i].GetComponent<Renderer>().sortingLayerName = sortLayerName;
+//                cardParts[0].GetComponent<Renderer>().sortingOrder = 1;
+//                cardParts[1].GetComponent<Renderer>().sortingOrder = 2;
+//                cardParts[2].GetComponent<Renderer>().sortingOrder = 2;
+//                cardParts[3].GetComponent<Renderer>().sortingOrder = 2;
+//                cardParts[4].GetComponent<Renderer>().sortingOrder = 2;
+//                cardParts[5].GetComponent<Renderer>().sortingOrder = 1;
+//                cardParts[6].GetComponent<Renderer>().sortingOrder = 2;
+//                cardParts[7].GetComponent<Renderer>().sortingOrder = 1;
+//            }
         }
     }
 
@@ -158,6 +212,45 @@ public class PlayerCardDisplay : CardDisplay
         currencyText.text = card.CardCurrency.ToString();
         LoadCostEffectIcons();
     }
+    
+    //increase the order of the icons inside the card once the card is highlighted
+    public void OnMouseEnter()
+    {
+        //TODO: Implement an incrementor for the sorting order of each sorting layer
+        Debug.Log("Enter betch!");
+//        cardParts[0].GetComponent<Renderer>().sortingOrder = 2;
+//        cardParts[1].GetComponent<Renderer>().sortingOrder = 3;
+//        cardParts[2].GetComponent<Renderer>().sortingOrder = 3;
+//        cardParts[3].GetComponent<Renderer>().sortingOrder = 3;
+//        cardParts[4].GetComponent<Renderer>().sortingOrder = 3;
+//        cardParts[5].GetComponent<Renderer>().sortingOrder = 2;
+//        cardParts[6].GetComponent<Renderer>().sortingOrder = 3;
+//        cardParts[7].GetComponent<Renderer>().sortingOrder = 2;
+        originalImageOrder += 1;
+        originalTypeOrder += 1;
+        originalAttackOrder += 1;
+        originalSoulOrder += 1;
+        originalCostOrder += 1;
+        originalBorderOrder += 1;
+        originaleffectOrder += 1;
+        originalNameOrder += 1;
+    }
+
+    public void OnMouseExit()
+    {
+        Debug.Log("Exit betch!");
+        //TODO: Return the sorting order back to its original value
+        
+        originalImageOrder -= 1;
+        originalTypeOrder -= 1;
+        originalAttackOrder -= 1;
+        originalSoulOrder -= 1;
+        originalCostOrder -= 1;
+        originalBorderOrder -= 1;
+        originaleffectOrder -= 1;
+        originalNameOrder -= 1;
+    }
+    
 
     /// <summary>
     /// Removes all references to the Card's information from this display.
