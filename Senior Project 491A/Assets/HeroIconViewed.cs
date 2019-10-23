35522f5b45a23c2e7dec2edcb5f3e8633bb1e3d2
Alex Pinedo <31834217+rocketmanhs@users.noreply.github.com>
2019-10-22 17:28:26 -0700

    pushing some final updates

---
+++ Senior Project 491A/Assets/HeroIconViewed.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HeroIconViewed : MonoBehaviour
+{
+    private HeroIconViewed display;
+
+    [SerializeField]
+    private TextMeshProUGUI heroPowerText;
+
+    private void Awake()
+    {
+        display = this;
+        display.gameObject.SetActive(false);
+
+        heroPowerText = GetComponentInChildren<TextMeshProUGUI>();
+        
+        //heroPowerText.text = "";
+
+    }
+
+    private void OnEnable()
+    {
+        TurnPlayerHeroManager.HeroChanged += SetHeroPowerText;
+    }
+
+    private void OnDisable()
+    {
+        TurnPlayerHeroManager.HeroChanged -= SetHeroPowerText;
+    }
+
+    private void Start()
+    {
+    }
+
+    public void ActivateDisplay()
+    {
+        display.gameObject.SetActive(true);
+    }
+
+    public void DecactivateDisplay()
+    {
+        display.gameObject.SetActive(false);
+    }
+
+    private void SetHeroPowerText()
+    {
+        heroPowerText.text = TurnPlayerHeroManager.Instance.ActiveTurnHero.HeroPowerMessageDisplay;
+    }
+}
