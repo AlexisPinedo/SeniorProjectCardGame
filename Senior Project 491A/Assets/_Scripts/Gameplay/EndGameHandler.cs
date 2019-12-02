387ec65ea9d8937dc461be2caf44cd1b711a7311
Alex Pinedo <31834217+AlexisPinedo@users.noreply.github.com>
2019-12-02 11:24:38 -0800

    end game action event added to EndGameHandler

--- Senior Project 491A/Assets/_Scripts/Gameplay/EndGameHandler.cs
+++ Senior Project 491A/Assets/_Scripts/Gameplay/EndGameHandler.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EndGameHandler : MonoBehaviour
 {
+    public static Action GameEnded;
+    
+    
     public static void TriggerEndGame()
     {
+        GameEnded?.Invoke();
         NotificationWindowEvent.Instance.EnableNotificationWindow("Game ended please restart scene");
     }
 }
